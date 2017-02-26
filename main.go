3e73cfb1d47a27d20bd6dd30b9f765671a369651
icodezjb <icodezjb@163.com>
2017-02-26 17:32:59 +0800

    support recursion dir and ignore the specified dir or/and files

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
+
+const DEBUG = true
 
 func file_sha1(filename string) string {
 	var ret string
@@ -30,57 +33,82 @@ func file_sha1(filename string) string {
 	return ret
 }
 
-func main() {
-
-	var outputWriter *bufio.Writer
-	var DIR string
-	flag.StringVar(&DIR, "dir", "./", "the destination directory")
-	flag.Parse()
-
-	dir_list, e := ioutil.ReadDir(DIR)
+func recursion_dir(dirname string, pattern *string, outputWriter *bufio.Writer) {
+	dir_list, e := ioutil.ReadDir(dirname)
 	if e != nil {
-		fmt.Printf("read the dir(DIR) error\n")
-		return
-	}
-	fmt.Println(DIR)
-	if nil == os.Chdir(DIR) {
-		/* reclear .result file */
-		outputfile, openerr := os.Create(".result")
-		if openerr != nil {
-			fmt.Println("open the result file fail!")
-			return
-		} else {
-			outputWriter = bufio.NewWriter(outputfile)
-			defer outputfile.Close()
-		}
-
-	} else {
-		fmt.Printf("can't enter this directory:%s\n ")
+		fmt.Printf("read the dir(%s) error\n", dirname)
 		return
 	}
 
 	for i, v := range dir_list {
+		isMatched, _ := filepath.Match(*pattern, v.Name())
+		if isMatched {
+			continue
+		}
 
 		var outputstring string
+		path := fmt.Sprintf("%s/%s", dirname, v.Name())
 
 		switch v.IsDir() {
 		case true:
-			fmt.Printf("%s is directory\n", v.Name())
-			break
+			if DEBUG {
+				fmt.Printf("%s is directory\n", v.Name())
+			}
+			recursion_dir(path, pattern, outputWriter)
 		case false:
-			/* skip .result file */
-			if ".result" == v.Name() {
+			/* skip thess files*/
+			if (".result" == v.Name()) || ((v.Mode() & os.ModeType) != 0) {
 				continue
 			}
 
-			hashstring := file_sha1(v.Name())
+			/* only deal with regular file */
+			hashstring := file_sha1(path)
 			if "" != hashstring {
 				/*wins:\r\n, unix:\n*/
-				outputstring = fmt.Sprintf("%d,%s,%s,%d\r\n", i, v.Name(), hashstring, v.Size())
+				outputstring = fmt.Sprintf("%d,%s,%s,%d,%t\r\n", i, v.Name(), hashstring, v.Size(), ((v.Mode() & os.ModeType) == 0))
 				outputWriter.WriteString(outputstring)
-				//fmt.Println(outputstring)
+				if DEBUG {
+					fmt.Println(outputstring)
+				}
 			}
 		}
+	}
+
+	return
+}
+
+func main() {
+
+	var outputWriter *bufio.Writer
+	var DIR string
+	var RESULT string
+	var PATTERN string
+
+	flag.StringVar(&DIR, "d", "./", "the destination directory")
+	flag.StringVar(&RESULT, "o", "./.result", "output path of the reslut file")
+	flag.StringVar(&PATTERN, "i", "", "the ignore dirs or/and files")
+
+	flag.Parse()
+
+	if DEBUG {
+		fmt.Println(DIR)
+		fmt.Println(RESULT)
+	}
+	/* reclear .result file */
+	outputfile, openerr := os.Create(RESULT)
+	if openerr != nil {
+		fmt.Println("open the result file fail!")
+		return
+	} else {
+		outputWriter = bufio.NewWriter(outputfile)
+		defer outputfile.Close()
+	}
+
+	isMatched, _ := filepath.Match(PATTERN, DIR)
+	if isMatched {
+		fmt.Println("ignore thr dir:%s", DIR)
+	} else {
+		recursion_dir(DIR, &PATTERN, outputWriter)
 	}
 
 	outputWriter.Flush()
